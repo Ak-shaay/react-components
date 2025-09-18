42ac4635a38a0c07754a089e156f4223e589a346
Ak-shaay <akshay1999achu@gmail.com>
2025-09-18 11:41:22 +0530

    initial commit

---
+++ src/Components/ClassComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class ClassComponent extends React.Component{
+    render(){
+        return <h2>This is a Class Component </h2>
+    }
+}
+export default ClassComponent;
---
+++ src/Components/FunctionalComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function FunctionalComponent(){
+    return(
+        <h2>This is s functional component</h2>
+    )
+}
+export default FunctionalComponent;
