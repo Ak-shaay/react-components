bec97ffe81ac13e4e54796b309e83268f319c358
Ak-shaay <akshay1999achu@gmail.com>
2025-09-19 12:06:10 +0530

    props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import FunctionalComponent from './Components/FunctionalComponent';
 import ClassComponent from './Components/ClassComponent';
 
 function App() {
+
+  const obj={
+    functional:"properties sent as properties = {obj}"
+  }
+
   return (
     <div className="App">
-     <FunctionalComponent/>
+     <FunctionalComponent properties = {obj}/>
      <ClassComponent/>
     </div>
   );
--- src/Components/FunctionalComponent.jsx
+++ src/Components/FunctionalComponent.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function FunctionalComponent(){
+function FunctionalComponent(props){
     return(
+        <>
         <h2>This is s functional component</h2>
+        <h2>The value from props is : <br/> {props.properties.functional}</h2>
+        </>
     )
 }
 export default FunctionalComponent;
